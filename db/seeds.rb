de980e4fc14fb1a7df79f6db0372f69967fa3353
Daniel Owens <danielodev@gmail.com>
2018-07-09 06:30:34 -0500

    Wrote script for seeding database with lorem events.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..32).each do
+	random_title = Faker::Lorem.sentence(word_count=rand(2..5))
+	start = Faker::Time.between(1.month.ago, 1.month.from_now)
+
+	# Randomly generates some number of minutes between 15-3.hours and rounds it to the nearest 5 minute mark.
+	minute_delta = (rand(15..(60*3)) / 5).ceil * 5
+	finished = start + minute_delta.minutes
+
+	Event.create(title: random_title, start: start, end: finished)
+end
