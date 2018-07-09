cb3ac996ba1243cf266b5e99e1364b24da62b1ed
Daniel Owens <danielodev@gmail.com>
2018-07-09 11:26:10 -0500

    Seed script uses variables that can be more easily changed now.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-(1..32).each do
+before_fill = 6.month.ago
+after_fill = 6.month.from_now
+total_events = 12 * (6 * 2 + 1)
+
+(1..total_events).each do
 	random_title = Faker::Lorem.sentence(word_count=rand(2..5))
-	start = Faker::Time.between(1.month.ago, 1.month.from_now)
+	start = Faker::Time.between(before_fill, after_fill)
 
 	# Randomly generates some number of minutes between 15-3.hours and rounds it to the nearest 5 minute mark.
 	minute_delta = (rand(15..(60*3)) / 5).ceil * 5
