b5a314f4616d465f8510f0e01da8a547f8c8dbe5
Daniel Owens <danielodev@gmail.com>
2018-07-09 05:01:01 -0500

    Fixed way attributes are accessed to avoid ForbiddenAttributesError.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
 	def create
-		event = Event.new params[:event]
+		event = Event.new params.require(:event).permit(:title, :start, :end)
 		if event.save
 			render json: event
 		end
