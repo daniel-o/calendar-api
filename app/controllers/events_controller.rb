761c31b2bbb934adc91cdeb006c0a8fd58802aeb
Daniel Owens <danielodev@gmail.com>
2018-07-09 06:06:45 -0500

    Setup simple query for finding Events in a date range.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,14 @@
 class EventsController < ApplicationController
+	def show
+		begin
+			start = params.require(:start)
+			tail = params.require(:end)
+			render json: Event.where(start: start..tail)
+		rescue ActionController::ParameterMissing
+			render json: {error: "Requires 'start' and 'end' parameters."}, status: 400
+		end
+	end
+
 	def create
 		event = Event.new params.require(:event).permit(:title, :start, :end)
 		if event.save
